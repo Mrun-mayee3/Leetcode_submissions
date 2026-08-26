8fdad999bc3a5a6aa8b64730e18134a040b93dd7
Mrunmayee <131985142+Mrun-mayee3@users.noreply.github.com>
2026-08-26 23:21:20 +0530

    Time: 0 ms (100%), Space: 35.8 MB (55.46%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
-         
-        
-        for(int i = 0; i < n; i++){
-            int l = 0, r = n-1;
-            int rsum =0 , lsum =0;    
+        // --------------------- Brute force
+        //int n = nums.size();
+        //for(int i = 0; i < n; i++){
+        //    int l = 0, r = n-1;
+        //    int rsum =0 , lsum =0;    
+//
+        //    while(l < i){
+        //        lsum += nums[l];
+        //        l++;
+        //    }
+        //    while(r > i){
+        //        rsum += nums[r];
+        //        r--;
+        //    }
+        //    if(lsum == rsum)
+        //        return i;
+        //}
+        //return -1;
 
-            while(l < i){
-                lsum += nums[l];
-                l++;
-            }
-            while(r > i){
-                rsum += nums[r];
-                r--;
-            }
-            if(lsum == rsum)
-                return i;
+        int total = 0;
+        for(int i = 0; i < nums.size(); i++){
+            total += nums[i];
+        }
+
+        int lsum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            int rsum = total - lsum - nums[i];
+            if(rsum == lsum) return i;
+            lsum += nums[i];
         }
         return -1;
     }
