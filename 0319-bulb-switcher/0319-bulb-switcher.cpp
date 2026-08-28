b679dcd23bfee7d2a0aee9b2fda478a4c821f315
Mrunmayee <131985142+Mrun-mayee3@users.noreply.github.com>
2026-08-29 00:05:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (2.76%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        int count = 0;
+
+        for(long long i = 1; i * i <= n; i++){
+            count++;
+        }
+        return count;
+    }
+};
