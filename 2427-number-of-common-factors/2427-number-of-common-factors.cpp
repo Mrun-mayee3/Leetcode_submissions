4eda00ae5e8de2581f1421a2ead8d7bb1ec416a9
Mrunmayee <131985142+Mrun-mayee3@users.noreply.github.com>
2026-08-28 19:16:31 +0530

    Time: 0 ms (100%), Space: 8.2 MB (2.95%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int total_num = 0;
+        vector<int> v;
+        
+        int small = min(a, b);
+        int large = max(a, b);
+        
+        for (int i = 1; i <= small/i; i++){
+            if(small % i == 0){
+                v.push_back(i);  
+                
+                if(i != small / i) 
+                    v.push_back(small / i);
+            } 
+        }
+        for(int num : v){
+            if(large % num == 0) total_num++;
+        }
+        return total_num;
+    }
+};
