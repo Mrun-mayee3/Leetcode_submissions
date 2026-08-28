78c0b2cd09503c5d78cfdcc6bbbf8398a6ddd560
Mrunmayee <131985142+Mrun-mayee3@users.noreply.github.com>
2026-08-28 19:44:02 +0530

    Time: 0 ms (100%), Space: 7.9 MB (49.49%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.cpp
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -7,16 +7,22 @@ public:
         int small = min(a, b);
         int large = max(a, b);
         
-        for (int i = 1; i <= small/i; i++){
-            if(small % i == 0){
-                v.push_back(i);  
-                
-                if(i != small / i) 
-                    v.push_back(small / i);
-            } 
-        }
-        for(int num : v){
-            if(large % num == 0) total_num++;
+        //for (int i = 1; i <= small/i; i++){
+        //    if(small % i == 0){
+        //        v.push_back(i);  
+        //        
+        //        if(i != small / i) 
+        //            v.push_back(small / i);
+        //    } 
+        //}
+        //for(int num : v){
+        //    if(large % num == 0) total_num++;
+        //}
+        //return total_num;
+
+        for(int i = 1; i <= small; i++){
+            if(small % i == 0 && large % i == 0)
+                total_num++;
         }
         return total_num;
     }
